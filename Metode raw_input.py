70ce14cfcb032c843db50a3f64af11041470f260
Archimondee <kikchiemedia@gmail.com>
2015-08-17 17:03:47 +0700

    Testing
    Testing

---
+++ Metode raw_input.py
@@ -0,0 +1,5 @@
+nama = raw_input("Ketikan nama lengkap anda: ") #Untuk menginput kedalam barisnya
+nama = "Gilang Aditya Rahman"
+
+if nama == "Gilang Aditya Rahman":
+    print "Nama saya adalah %s" % nama
---
+++ metode whilee.py
@@ -0,0 +1,6 @@
+x = 10
+while x !=0:
+    print x
+    x = x - 1
+    print "Mari kita menghitung mundur, dimulai dari", 0
+print "Semuanya telah selesai"
